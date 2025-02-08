ed3be3287b6fafd748fe3096ef8aa838ed58ed39
Jason Sawiris <jasonsawiris@gmail.com>
2025-02-08 11:42:16 -0800

    Fixed Motor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,20 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.Constants;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Intake {
-  /** Creates a new Intake. */
   TalonFX intakeM;
+
   public Intake() {
-    intakeM = new TalonFX(16);//MOTOR ID
-    // Use addRequirements() here to declare subsystem dependencies.
+    intakeM = new TalonFX(Constants.IntakeVals.motorID);
   }
-  public void spin(double PercentOut){
-    intakeM.setVoltage(-PercentOut);
-    
+  public void Spin(double voltageOut){
+    intakeM.setVoltage(voltageOut);
   }
 }
