d9108ac652ad948d6f5a01ec0d1df878d9fa60b3
Jason Sawiris <jasonsawiris@gmail.com>
2025-02-08 11:52:07 -0800

    Fixed ALL

--- src/main/java/frc/robot/commands/TeleopIntake.java
+++ src/main/java/frc/robot/commands/TeleopIntake.java
@@ -1,39 +1,27 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+
 
 package frc.robot.commands;
 import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class TeleopIntake extends Command {
   /** Creates a new TeleopIntake. */
-  public TeleopIntake() {
-    Intake intake;
-    double PercentOut;
-    public TeleopIntake(Intake intake, double PercentOut){
-        this.intake = intake;
-        this.PercentOut = PercentOut;
-    }
-    // Use addRequirements() here to declare subsystem dependencies.
+  Intake intake;
+  double voltageOut;
+  public TeleopIntake(Intake intake, double voltageOut) {
+    this.intake = intake;
+    this.voltageOut = voltageOut;
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Intake.spin(PercentOut);
+    intake.Spin(voltageOut);
   }
-
-  // Called once the command ends or is interrupted.
+/* */
+  @Override
+  public void initialize() {}
   @Override
   public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
